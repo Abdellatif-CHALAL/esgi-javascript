b5d472d09f0287448e5101fb9f1bd8f273c3fc51
Abdellatif-CHALAL <57263914+Abdellatif-CHALAL@users.noreply.github.com>
2021-01-09 12:44:42 +0100

    Update pilote.js

--- exericce-4/pilote.js
+++ exericce-4/pilote.js
@@ -1,38 +1,59 @@
-var pilote = function Pilote(name) {
-    const props = {};
+function Pilote(name) {
     this.name = name;
     const props = {};
+    let prevProps = {};
+  
     this.receiveData = function (data) {
-            props.state = data.state;   
-            props.position = data.position;
-            props.origin = data.origin;
-    }
-
-    this.getState = function () {
-        return state.state; 
-    }
+      prevProps = Object.assign({}, props);
+      props.state = data.state;
+      props.origin = data.origin;
+      props.position = data.position;
+    };
+  
+    this.getState = () => props.state;
+  
     this.speak = function () {
-        switch (props.state) {
-            case "ready": return `Here we go! I'm ${state.origin}`;break;
-            case "happy": return "Let's have some fun!";break;
-            case "sad": return `Outch!!! Damn ${state.origin}`;break;
-            case "ready": return "Here we go!";break;
-            case "finish": switch(state.position){
-                case 1 : return "I'm the best";
-                case 2 : return "Could be the best";
-                default : return "Will be better next time"; 
-            }
+      switch (props.state) {
+        case "ready":
+          return `Here we go! I'm ${this.name}`;
+        case "happy":
+          return "Let's have some fun!";
+        case "sad":
+          return `Outch!!! Damn ${props.origin}`;
+        case "normal":
+          return "";
+        case "finish":
+          switch (props.position) {
+            case 1:
+              return "I'm the best";
+            case 2:
+              return "Could be the best";
             default:
-                break;
-        }
-    }
-
-    this.needUpdate = function() {
-        return JSON.stringify(k1) === JSON.stringify(k2);
-    }
- }
- 
- console.log(new Pilote("latif"));
+              return "Will be better next time";
+          }
+      }
+    };
+  
+    this.needUpdate = () => JSON.stringify(prevProps) !== JSON.stringify(props);
+  }
+  
+  const pilote = new Pilote("Mario");
+  
+  pilote.receiveData({ state: "ready" });
+  if (pilote.needUpdate()) console.log("Speak ready", pilote.speak());
+  pilote.receiveData({ state: "normal" });
+  if (pilote.needUpdate()) console.log("Speak normal", pilote.speak());
+  pilote.receiveData({ state: "normal" });
+  if (pilote.needUpdate()) console.log("Speak normal", pilote.speak());
+  pilote.receiveData({ state: "happy" });
+  if (pilote.needUpdate()) console.log("Speak happy", pilote.speak());
+  pilote.receiveData({ state: "sad", origin: "Luigi" });
+  if (pilote.needUpdate()) console.log("Speak sad", pilote.speak());
+  pilote.receiveData({ state: "finish", position: 1 });
+  if (pilote.needUpdate()) console.log("Speak finish", pilote.speak());
+  pilote.receiveData({ state: "finish", position: 1 });
+  if (pilote.needUpdate()) console.log("Speak finish", pilote.speak());
+  
 
 
 
